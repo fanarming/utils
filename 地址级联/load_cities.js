1f052f55f60f8e206accb7daac40c4a81d857c31
fanarming <fanarming@gmail.com>
2016-12-26 15:43:06 +0800

    更新

--- 地址级联/load_cities.js
+++ 地址级联/load_cities.js
@@ -1,6 +1,11 @@
 ﻿;(function($){
-	var Loadcities = function(ele){
+	var Loadcities = function(ele,options){
 		this.$element = ele;
+		this.options = options;
+		this.initPro = options.province ? options.province : false;
+		this.initProDeflt = options.prodefault ? options.prodefault : false;
+		this.initCity = options.city ? options.city : false;
+		this.initCityDeflt = options.citydefault ? options.citydefault : false;
 		this.$chinese_cities = $.CHINESE_CITIES();		
 	};
 
@@ -16,70 +21,86 @@
 		},
 
 		_changeAction: function(){
-			var cities = this.$chinese_cities,//载入地址列表数据
-//console.log(cities);
+			var self = this,
+				cities = self.$chinese_cities,//载入地址列表数据
+//console.log(cities);				
 			    $province = $("#province"),
 				$cities = $("#city"),
 				$region = $("#region");
+		
 
-			$.each(cities, function(index, item){//初始化省份
+			$.map(cities, function(item, index){//初始化省份
 /*console.log("第"+index+"个数组是：");
 console.log(item);*/
-				$province.append("<option value='" + item[0] + "'>" + item[1] + "</option>");//单双引号相互嵌套，但不能相似的引号嵌套
+				$province.append("<option value=" + item[0] + " " + (item[0] == self.initPro ? 'selected' : '') + ">" + item[1] + "</option>");//单双引号相互嵌套，但不能相似的引号嵌套
 			});
 
-			$cities.attr('disabled', 'disabled').css({
-				'background': '#DCDCDC',
-				'cursor': 'not-allowed'
-			});
+			if(!$province.find("option:selected").length) {
+				$cities.attr('disabled', 'disabled').css({
+					'background': '#DCDCDC',
+					'cursor': 'not-allowed'
+				});
+			} else {
+				self.initProDeflt ? $province.attr('disabled','disabled') : '';
+				self._searchCities();	
+			}
+			
 
 			$province.change(function(event){// 改变省份
 /*console.log($(this).children("option:selected").val());
 console.log($(this).children("option:selected").text());*/
+				self._searchCities();				
+			});
+		},
 
-				var val = $(this).children("option:selected").val();
+		_searchCities: function(){
+			var self = this,
+				$province = $("#province"),
+				$cities = $("#city"),
+				$region = $("#region"),
+				val = $province.children("option:selected").val();
 
-				$cities.children('option:gt(0)').remove();
+			$cities.children('option:gt(0)').remove();
 
-				if (parseInt(val)) {					
-					var newarr = $.map(cities, function(item, index){
-						
-						var i = $.inArray(parseInt(val), item, 0);
+			if (parseInt(val)) {					
+				var newarr = $.map(self.$chinese_cities, function(item, index){
+					
+					var i = $.inArray(parseInt(val), item, 0);
 
-						if (i > -1) {
-							item.push('true'); //做标记
-							return item; //返回所在数组,如果还有区需要选择可以在新返回的数组里进行操作							
-						}
-					});
+					if (i > -1) {
+						item.push('true'); //做标记
+						return item; //返回所在数组,如果还有区需要选择可以在新返回的数组里进行操作							
+					}
+				});
 
 //console.log(newarr);
-					if (newarr[3]) {
-						$cities.removeAttr('disabled').css({
-							'background': '#fff',
-							'cursor': 'default'
-						});
-
-						$.each(newarr[2], function(index, item){
-							$cities.append("<option value='" + item[0] + "'>" + item[1] + "</option>");
-						});
-					}
-					
-				}  else {
-					$cities.attr('disabled', 'disabled').css({
-						'background': '#DCDCDC',
-						'cursor': 'not-allowed'
+				if (newarr[3]) {
+					$cities.removeAttr('disabled').css({
+						'background': '#fff',
+						'cursor': 'default'
 					});
 
-					$cities.children('option:gt(0)').remove();
-				};
+					$.each(newarr[2], function(index, item){
+						$cities.append("<option value=" + item[0] + " " + (item[0] == self.initCity ? 'selected' : '') + ">" + item[1] + "</option>");
+					});
+
+					self.initCityDeflt ? $cities.attr('disabled','disabled') : '';
+				}
 				
-			});
+			}  else {
+				$cities.attr('disabled', 'disabled').css({
+					'background': '#DCDCDC',
+					'cursor': 'not-allowed'
+				});
+
+				$cities.children('option:gt(0)').remove();
+			};
 		}
 	};
 
 
-	$.fn.loadCities = function (){
-		var loadcities = new Loadcities(this);
+	$.fn.loadCities = function (options){
+		var loadcities = new Loadcities(this,options);
 
 		loadcities._renderUI();
 		loadcities._changeAction();
